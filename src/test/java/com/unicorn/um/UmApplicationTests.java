44d121ae3d90a588c36c2fcd0c61b3ed7f704be9
unicorn <1334083811@qq.com>
2020-12-16 22:14:32 +0800

    自动生成实体

--- src/test/java/com/unicorn/um/UmApplicationTests.java
+++ src/test/java/com/unicorn/um/UmApplicationTests.java
@@ -1,23 +1,16 @@
 package com.unicorn.um;
 
-import com.unicorn.um.entity.User;
-import com.unicorn.um.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 @SpringBootTest
 class UmApplicationTests {
 
-    @Autowired
-    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
-        List<User> userList = userMapper.selectList(null);
-        System.out.println(userList);
+//        List<User> userList = userMapper.selectList(null);
+//        System.out.println(userList);
 
     }
 
