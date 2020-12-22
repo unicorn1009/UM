0039d8e23435f972662f8df262b876fc0f2a718a
unicorn1009 <1334083811@qq.com>
2020-12-23 00:05:25 +0800

    add login

---
+++ src/main/java/com/unicorn/um/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.unicorn.um.controller;
+
+import com.unicorn.um.common.R;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Locale;
+
+@RestController
+@CrossOrigin
+public class LoginController {
+
+    @PostMapping("/login")
+    public R login(@RequestBody Object obj) {
+        Object username = ((LinkedHashMap) obj).get("username");
+        Object pwd = ((LinkedHashMap) obj).get("password");
+        if (username.toString().toLowerCase().equals("admin") && pwd.toString().toLowerCase().equals("123"))
+            return R.ok().message("登陆成功").data("abc", "我是返回的数据");
+        else
+            return R.error().message("登录失败");
+    }
+}
